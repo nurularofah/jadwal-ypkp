5e8784c701b10a627a7f027647010ad92c0d8a7b
aseproniirawan <ronycoy63@yahoo.com>
2014-05-23 23:52:10 +0700

    Tabel

--- application/views/depan/beranda.php
+++ application/views/depan/beranda.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,14 +13,45 @@
 	  <div class="container-fluid">
 	    <!-- Brand and toggle get grouped for better mobile display -->
 	    <div class="navbar-header">	      
-	      <a class="navbar-brand" href="#">UNIVERSITAS SANGGA BUANA</a>
-	    </div>	    
-	      <ul class="nav navbar-nav navbar-right">	 
-	      	<p class="navbar-text">Tanggal</p>
-	      </ul>
+	      <a class="navbar-brand" href="#">UNIVERSITAS SANGGA BUANA</a>	 	      
 	    </div><!-- /.navbar-collapse -->
+	    <ul class="nav navbar-nav navbar-right nav-pills">
+	      	<li><p class="navbar-text "><?php echo date('D, d-M-Y'); ?></p></li>	            
+	    </ul>  
 	  </div><!-- /.container-fluid -->
 	</nav>
+
+	<div class="panel panel-primary">
+	  <!-- Default panel contents -->
+	  <div class="panel-heading">
+	  	<center><b>JADWAL PERKULIAHAN</b></center>
+	  </div>
+	  <!-- Table -->
+	  <div class="table-responsive">
+	  	<table class="table table-hover table-bordered">	  	
+	  	<thead>
+	  		<tr class="success">
+	  			<th>Mata Kuliah</th>
+	  			<th>Kelas</th>
+	  			<th>Waktu</th>
+	  			<th>Ruangan</th>
+	  			<th>Dosen</th>
+	  			<th>Status Dosen</th>
+	  		</tr>
+	  	</thead>
+	  	<tbody>
+	  		<tr>
+	  			<td>data</td>
+	  			<td>data</td>
+	  			<td>data</td>
+	  			<td>data</td>
+	  			<td>data</td>
+	  			<td>data</td>
+	  		</tr>
+	  	</tbody>
+	  </table>
+	  </div>	  	 
+	</div>
 
 </body>
 </html>
