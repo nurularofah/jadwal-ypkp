90cce8d21942ba60a4959933b472452fbb15d64e
aseproniirawan <ronycoy63@yahoo.com>
2014-06-06 23:39:14 +0700

    Hilangkan Scroll bar ketika F11

--- application/views/depan/beranda.php
+++ application/views/depan/beranda.php
@@ -9,20 +9,22 @@
 	<script src="assets/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<nav class="navbar navbar-inverse" role="navigation">
-	  <div class="container-fluid">
-	    <!-- Brand and toggle get grouped for better mobile display -->
-	    <div class="navbar-header">	      
-	      <a class="navbar-brand" href="#">UNIVERSITAS SANGGA BUANA</a>	 	      
-	    </div><!-- /.navbar-collapse -->
-	    <ul class="nav navbar-nav navbar-right nav-pills">
-	      	<li><p class="navbar-text "><?php echo date('l, d-M-Y'); ?></p></li>	            
-	    </ul>  
-	  </div><!-- /.container-fluid -->
-	</nav>
 
-
-
+	<!-- Navigation -->
+	<div class="container-fluid">
+		<nav class="navbar navbar-inverse" role="navigation">
+		  <div class="container-fluid">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">	      
+		      <a class="navbar-brand" href="#">UNIVERSITAS SANGGA BUANA</a>	 	      
+		    </div><!-- /.navbar-collapse -->
+		    <ul class="nav navbar-nav navbar-right nav-pills">
+		      	<li><p class="navbar-text "><?php echo date('l, d-M-Y'); ?></p></li>	            
+		    </ul>  
+		  </div><!-- /.container-fluid -->
+		</nav>
+	</div>
+	
 
 
 	<!-- Table -->
@@ -98,63 +100,69 @@
 
 
 	<!-- Berita dan Slide -->
-	<div class="row">
-		<div class="col-md-6">
-			<div id="nurul-slide" class="carousel slide" data-ride="carousel">
-			  <!-- Indicators -->
-			  <ol class="carousel-indicators">
-			    <li data-target="#nurul-slide" data-slide-to="0" class="active"></li>
-			    <li data-target="#nurul-slide" data-slide-to="1"></li>
-			    <li data-target="#nurul-slide" data-slide-to="2"></li>
-			    <li data-target="#nurul-slide" data-slide-to="3"></li>
-			  </ol>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-6">
+				<div id="nurul-slide" class="carousel slide" data-ride="carousel">
+				  <!-- Indicators -->
+				  <ol class="carousel-indicators">
+				    <li data-target="#nurul-slide" data-slide-to="0" class="active"></li>
+				    <li data-target="#nurul-slide" data-slide-to="1"></li>
+				    <li data-target="#nurul-slide" data-slide-to="2"></li>
+				    <li data-target="#nurul-slide" data-slide-to="3"></li>
+				  </ol>
 
-			  <!-- Wrapper for slides -->
-			  <div class="carousel-inner">
-			    <div class="item active">
-			      <img src="images/slide-1.jpg" alt="Gambar 1">	      
-			      <div class="carousel-caption">
-			        <h3>Gambar 1</h3>
-			      </div>	      
-			    </div>
-			    <div class="item">
-			      <img src="images/slide-2.jpg" alt="Gambar 2">	      
-			      <div class="carousel-caption">
-			        <h3>Gambar 2</h3>
-			      </div>
-			    </div>
-			    <div class="item">
-			      <img src="images/slide-3.jpg" alt="Gambar 3">	      
-			      <div class="carousel-caption">
-			        <h3>Gambar 3</h3>
-			      </div>
-			    </div>
-			    <div class="item">
-			      <img src="images/slide-4.jpg" alt="Gambar 4">	      
-			      <div class="carousel-caption">
-			        <h3>Gambar 4</h3>
-			      </div>
-			    </div>	    	    
-			  </div>
+				  <!-- Wrapper for slides -->
+				  <div class="carousel-inner">
+				    <div class="item active">
+				      <img src="images/slide-1.jpg" alt="Gambar 1">	      
+				      <div class="carousel-caption">
+				        <h3>Gambar 1</h3>
+				      </div>	      
+				    </div>
+				    <div class="item">
+				      <img src="images/slide-2.jpg" alt="Gambar 2">	      
+				      <div class="carousel-caption">
+				        <h3>Gambar 2</h3>
+				      </div>
+				    </div>
+				    <div class="item">
+				      <img src="images/slide-3.jpg" alt="Gambar 3">	      
+				      <div class="carousel-caption">
+				        <h3>Gambar 3</h3>
+				      </div>
+				    </div>
+				    <div class="item">
+				      <img src="images/slide-4.jpg" alt="Gambar 4">	      
+				      <div class="carousel-caption">
+				        <h3>Gambar 4</h3>
+				      </div>
+				    </div>	    	    
+				  </div>
 
-			  <!-- Controls -->
-			  <a class="left carousel-control" href="#nurul-slide" data-slide="prev">
-			    <span class="glyphicon glyphicon-chevron-left"></span>
-			  </a>
-			  <a class="right carousel-control" href="#nurul-slide" data-slide="next">
-			    <span class="glyphicon glyphicon-chevron-right"></span>
-			  </a>
+				  <!-- Controls -->
+				  <a class="left carousel-control" href="#nurul-slide" data-slide="prev">
+				    <span class="glyphicon glyphicon-chevron-left"></span>
+				  </a>
+				  <a class="right carousel-control" href="#nurul-slide" data-slide="next">
+				    <span class="glyphicon glyphicon-chevron-right"></span>
+				  </a>
+				</div>
 			</div>
-		</div>
- 		<div class="col-md-4">Berita</div>
-	</div><br>
+	 		<div class="col-md-4">Berita</div>
+		</div><br>
+	</div>
+	
 
 	
 	<!-- Running Text -->
-	<div class="alert alert-success">
-		<marquee direction="right">
-			Nurul Arofah
-		</marquee>
+	<div class="container-fluid">
+		<div class="alert alert-success">
+			<marquee direction="right">
+				Nurul Arofah
+			</marquee>
+		</div>
 	</div>
+	
 </body>
 </html>
