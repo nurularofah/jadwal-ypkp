6308a6aacda73251d29a83bba5b559af6ee94fe3
Roni <ronycoy63@yahoo.com>
2014-06-08 17:45:18 +0700

    Setting URL htaccess

---
+++ application/controllers/pengaturan.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Pengaturan extends CI_Controller {
+
+	public function pengaturan()
+	{
+		echo phpinfo();
+	}
+
+}
+
+/* End of file pengaturan.php */
+/* Location: ./application/controllers/pengaturan.php */
