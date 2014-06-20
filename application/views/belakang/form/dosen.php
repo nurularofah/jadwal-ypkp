92099bb76a53408f833a63b7991164b3fc4291cd
aseproniirawan <ronycoy63@yahoo.com>
2014-06-20 07:31:15 +0700

    Setting Modal

--- application/views/belakang/form/dosen.php
+++ application/views/belakang/form/dosen.php
@@ -10,8 +10,7 @@
 		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 		  <div class="modal-dialog">
 		    <div class="modal-content">
-		      <div class="modal-header">
-		        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+		      <div class="modal-header">		        
 		        <h4 class="modal-title" id="myModalLabel">Data Dosen</h4>
 		      </div>
 		      <div class="modal-body">
